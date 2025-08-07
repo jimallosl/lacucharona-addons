9016758d4ea445d4ee1e91ef1bf9f522a3b90722
jimallosl <jimallosl@gmail.com>
2025-08-07 14:18:30 +0100

    Create redsys.py

---
+++ payment_redsys/models/redsys.py
@@ -0,0 +1,6 @@
+from odoo import models
+
+class PaymentAcquirerRedsys(models.Model):
+    _inherit = 'payment.acquirer'
+
+    # Aquí irán los campos específicos de configuración de Redsys más adelante
