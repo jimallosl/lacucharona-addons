636521097c4a1a9b422dea29fc6dc31a6b5c46f4
jimallosl <jimallosl@gmail.com>
2025-08-07 14:10:19 +0100

    Create __manifest__.py

---
+++ payment_redsys/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Redsys Payment Acquirer",
+    'version': '16.0.1.0.0',
+    'category': 'Accounting',
+    'summary': "Integración con Redsys para pagos online",
+    'description': "Módulo de pago Redsys para Odoo.sh",
+    'author': "La Cucharona",
+    'website': "https://lacucharonaparallevar.es",
+    'depends': ['payment'],
+    'data': [
+        # Aquí irán los archivos XML más adelante
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
