e2faf9c776ba4641b9a944c227e554e920b00b53
jimallosl <jimallosl@gmail.com>
2025-08-07 14:21:40 +0100

    Update main.py

--- payment_redsys/controllers/main.py
+++ payment_redsys/controllers/main.py
@@ -1,9 +1,22 @@
+# payment_redsys/controllers/main.py
+
 from odoo import http
 from odoo.http import request
+import logging
+
+_logger = logging.getLogger(__name__)
 
 class RedsysController(http.Controller):
 
     @http.route(['/payment/redsys/return'], type='http', auth='public', csrf=False)
     def redsys_return(self, **post):
-        # Aquí se manejará la respuesta del TPV Redsys
-        return request.redirect('/payment/process')
+        """ Página a la que se redirige al usuario después del pago """
+        _logger.info("Redsys return: %s", post)
+        return request.render("payment_redsys.redsys_return_page", {})
+
+    @http.route(['/payment/redsys/notify'], type='http', auth='public', csrf=False)
+    def redsys_notify(self, **post):
+        """ Notificación directa de Redsys """
+        _logger.info("Redsys notify: %s", post)
+        # Aquí debes validar la firma, actualizar el estado del pedido, etc.
+        return 'OK'
