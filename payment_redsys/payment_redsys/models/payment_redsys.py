c510e7ed4a6eafc6d28610fe4fc27c0b70aeaf9a
jimallosl <jimallosl@gmail.com>
2025-08-12 13:14:57 +0200

    Delete payment_redsys/payment_redsys/models/payment_redsys.py
    chore(redsys): remove nested payment_redsys duplicate

--- payment_redsys/payment_redsys/models/payment_redsys.py
+++
@@ -1,9 +0,0 @@
-from odoo import models, fields
-
-class PaymentAcquirerRedsys(models.Model):
-    _inherit = 'payment.acquirer'
-
-    provider = fields.Selection(selection_add=[('redsys', 'Redsys')])
-    redsys_merchant_code = fields.Char(string="Merchant Code")
-    redsys_secret_key = fields.Char(string="Secret Key")
-    redsys_terminal = fields.Char(string="Terminal")
